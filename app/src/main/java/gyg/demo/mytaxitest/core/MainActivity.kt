13347b31b4dc83972e0812ec736c573b98b0f5f3
Nami <namnamsalimi@gmail.com>
2019-05-01 02:36:24 +0700

    Set up MainActivity to add the TaxiListFragment to the activity

--- app/src/main/java/gyg/demo/mytaxitest/core/MainActivity.kt
+++ app/src/main/java/gyg/demo/mytaxitest/core/MainActivity.kt
@@ -1,48 +1,27 @@
 package gyg.demo.mytaxitest.core
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import gyg.demo.mytaxitest.R
-import gyg.demo.mytaxitest.data.TaxiService
-import gyg.demo.mytaxitest.data.model.hasData
-import gyg.demo.mytaxitest.providers.BaseScheduleProvider
-import gyg.demo.mytaxitest.taxiList.data.Hamburg
-import io.reactivex.Observable
-import javax.inject.Inject
+import gyg.demo.mytaxitest.taxiList.TaxiListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var service: TaxiService
-
-    @Inject
-    lateinit var scheduleProvider: BaseScheduleProvider
+    private val taxiListFragment: TaxiListFragment by lazy { TaxiListFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
         setContentView(R.layout.activity_main)
 
-        Observable.just(Hamburg())
-            .flatMap {
-                service.getTaxis(
-                    it.bound1.lat,
-                    it.bound1.long,
-                    it.bound2.lat,
-                    it.bound2.long
-                )
-            }
-            .subscribeOn(scheduleProvider.io())
-            .observeOn(scheduleProvider.ui())
-            .subscribe({
-                Log.d("TaxiType has data", it.hasData().toString())
-                Log.d("TaxiType data: ", it.list[4].toString())
-            }, {
-                it.printStackTrace()
-                Log.d("TAXI_NO_DATA", it.localizedMessage)
-            })
+        startTaxiListFragment()
+    }
 
+    private fun startTaxiListFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_activity_container, taxiListFragment)
+            .commit()
     }
 }
--- app/src/main/java/gyg/demo/mytaxitest/taxiList/TaxiListFragment.kt
+++ app/src/main/java/gyg/demo/mytaxitest/taxiList/TaxiListFragment.kt
@@ -1,6 +1,43 @@
 package gyg.demo.mytaxitest.taxiList
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import gyg.demo.mytaxitest.core.BaseFragment
+import gyg.demo.mytaxitest.data.ResultWrapper
 
-class TaxiListFragment : Fragment() {
+class TaxiListFragment : BaseFragment() {
+
+    var viewModel: TaxiListViewModel? = null
+
+    var a = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (viewModel == null) {
+            viewModel = ViewModelProviders.of(this, viewModelFactory).get(TaxiListViewModel::class.java)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel?.let { vm ->
+            vm.getInitTaxis()
+            vm.data.observe(this, Observer {
+                a = when (it) {
+                    is ResultWrapper.Success -> {
+                        true
+
+                    }
+                    is ResultWrapper.Failure -> {
+                        false
+                    }
+                }
+            })
+        }
+
+    }
 }
