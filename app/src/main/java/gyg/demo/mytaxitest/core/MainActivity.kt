c69e34619c984e86a995e01442aca8c1e7ed3149
Nami <namnamsalimi@gmail.com>
2019-05-03 00:12:02 +0700

    Abstracted some basic Activity logic into BaseActivity

--- app/src/main/java/gyg/demo/mytaxitest/core/MainActivity.kt
+++ app/src/main/java/gyg/demo/mytaxitest/core/MainActivity.kt
@@ -3,21 +3,17 @@ package gyg.demo.mytaxitest.core
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import dagger.android.AndroidInjection
 import gyg.demo.mytaxitest.R
 import gyg.demo.mytaxitest.taxiList.TaxiListFragment
-import gyg.demo.mytaxitest.taxiMap.TaxiMapActivity
 import gyg.demo.mytaxitest.taxiMap.startTaxiMapActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     private val taxiListFragment: TaxiListFragment by lazy { TaxiListFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AndroidInjection.inject(this)
         setContentView(R.layout.activity_main)
 
         startTaxiListFragment()
@@ -36,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId == R.id.map_option){
+        if (item?.itemId == R.id.map_option) {
             startTaxiMapActivity()
         }
 
