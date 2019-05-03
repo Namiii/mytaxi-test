cabb04f2c8614c4c72b6ea0028dcec5d06968b42
Nami <namnamsalimi@gmail.com>
2019-05-03 22:24:23 +0700

    Created separate drawables for different taxi types

--- app/src/main/java/gyg/demo/mytaxitest/core/DatabindingAdapters.kt
+++ app/src/main/java/gyg/demo/mytaxitest/core/DatabindingAdapters.kt
@@ -1,14 +1,8 @@
 package gyg.demo.mytaxitest.core
 
-import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.PorterDuff
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.ColorRes
-import androidx.core.content.ContextCompat
-import androidx.core.widget.ImageViewCompat
 import androidx.databinding.BindingAdapter
 import gyg.demo.mytaxitest.R
 import gyg.demo.mytaxitest.data.model.TaxiType
@@ -17,16 +11,12 @@ import gyg.demo.mytaxitest.data.model.Vehicle
 @BindingAdapter("setTaxiIcon")
 fun setTaxiIcon(view: ImageView, vehicle: Vehicle?) {
     vehicle?.let {
-        val colorFilterValue = when (vehicle.type) {
-            is TaxiType.NormalTaxi -> {
-                R.color.normal_taxi_icon_color
+        view.setImageResource(
+            when (vehicle.type) {
+                TaxiType.NormalTaxi -> R.drawable.ic_vehicle_normal
+                TaxiType.PoolingTaxi -> R.drawable.ic_vehicle_pooling
             }
-            is TaxiType.PoolingTaxi -> {
-                R.color.pooling_taxi_icon_color
-            }
-        }
-        view.setTint(view.context, colorFilterValue)
-        view.setImageResource(R.drawable.ic_vehicle)
+        )
     }
 }
 
@@ -48,10 +38,4 @@ fun setTaxiType(view: TextView, vehicle: Vehicle?) {
 @BindingAdapter("goneIf")
 fun goneIf(view: View, gone: Boolean) {
     view.visibility = if (gone) View.GONE else View.VISIBLE
-}
-
-fun ImageView.setTint(context: Context, @ColorRes colorId: Int) {
-    val color = ContextCompat.getColor(context, colorId)
-    val colorStateList = ColorStateList.valueOf(color)
-    ImageViewCompat.setImageTintList(this, colorStateList)
 }
